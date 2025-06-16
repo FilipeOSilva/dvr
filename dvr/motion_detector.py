8a330d0211c56e209fc92981de4146311acbeed1
Filipe Silva <filipe.ufsc@gmail.com>
2025-06-15 22:46:23 -0300

    feat: test for rasp using cv2 for detect mov

--- dvr/motion_detector.py
+++ dvr/motion_detector.py
@@ -4,10 +4,10 @@ import time
 import cv2
 from bot_telegram import BotTelegram
 from path_file import dir_path
-from ultralytics import YOLO
 from video_recorder import VideoRecorder
 
 MAX_DELAY = 15
+MIN_MOVIMENT = 1000
 
 
 class MotionDetector:
@@ -23,13 +23,13 @@ class MotionDetector:
         self._video_record = VideoRecorder(self._rtsp_url)
         self._chat_id: str = chat_id
         self._token_telegram: str = token_telegram
+        self._old_frame = None
 
     def _start_time(self) -> None:
         self._total_time_record = int(time.time())
 
     def _total_time(self) -> bool:
         now = int(time.time())
-
         if (now - self._total_time_record) > MAX_DELAY:
             return True
         else:
@@ -47,29 +47,48 @@ class MotionDetector:
         bot = BotTelegram(self._token_telegram, self._chat_id)
         bot.send_message('Movimento Detectado!')
 
+    def _check_moviment(self, img) -> bool:
+        frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        frame = cv2.GaussianBlur(frame, (21, 21), 0)
+        diff = cv2.absdiff(self._old_frame, frame)
+        _, thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        self._old_frame = frame.copy()
+        for contour in contours:
+            if cv2.contourArea(contour) < MIN_MOVIMENT:
+                continue
+
+            return True
+
+        return False
+
     def start_motion(self) -> None:
-        model = YOLO('yolov8n.pt')
         cap = cv2.VideoCapture(self._rtsp_url)
 
         if not cap.isOpened():
             sys.exit()
 
         try:
+            ret, frame = cap.read()
+            self._old_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            self._old_frame = cv2.GaussianBlur(self._old_frame, (21, 21), 0)
+
             while True:
                 ret, frame = cap.read()
-                if not ret:
-                    continue
 
-                info_image = model(frame)
-                for objs in info_image:
-                    obj = objs.boxes
-                    for datas in obj:
-                        if datas.cls[0] == 0:
-                            if not self._is_record:
-                                self._start_record()
-                                self._send_msg()
-                            self._is_record = True
-                            self._start_time()
+                if not ret:
+                    print('Erro em caputar frame!')
+                    break
+
+                if self._check_moviment(frame):
+                    if not self._is_record:
+                        self._start_record()
+                        self._send_msg()
+                    self._is_record = True
+                    self._start_time()
 
                 if self._is_record:
                     if self._total_time():
